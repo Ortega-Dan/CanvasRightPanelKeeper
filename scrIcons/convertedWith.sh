6a98fa49c849ca28beacf4434e5aba973afc0264
Dan Ortega <ortega.dan2010@gmail.com>
2021-01-19 00:40:21 -0500

    1.0.0

---
+++ scrIcons/convertedWith.sh
@@ -0,0 +1,1 @@
+gm convert -background none input.svg -resize 512 output.png
---
+++ src/content.js
@@ -0,0 +1,18 @@
+
+
+// Listening to keypress events in the entire document
+$("html").keypress(function (event) {
+
+    // Running functionality con Ctrl + i or Ctrl + k (case insensitive)
+    if (event.ctrlKey === true && event.code === "KeyI") {
+
+        console.log('CANVAS')
+        document.getElementById('main').style.width = '58vw'
+        var rightWrap = document.getElementById('right-side-wrapper')
+        rightWrap.style.position = 'fixed'
+        rightWrap.style.right = '10px'
+        rightWrap.style.top = '-10px'
+
+    }
+
+})
