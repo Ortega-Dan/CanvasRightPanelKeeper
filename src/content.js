d3cd5985723f1383bb92615df4f0b301039241c6
Dan Ortega <ortega.dan2010@gmail.com>
2023-03-25 23:37:16 -0500

    improved margins and upgrade to manifest version 3

--- src/content.js
+++ src/content.js
@@ -7,11 +7,11 @@ $("html").keypress(function (event) {
     if (event.ctrlKey === true && event.code === "KeyI") {
 
         console.log('CANVAS')
-        document.getElementById('main').style.width = '58vw'
+        document.getElementById('main').style.width = '68vw'
         var rightWrap = document.getElementById('right-side-wrapper')
         rightWrap.style.position = 'fixed'
         rightWrap.style.right = '10px'
-        rightWrap.style.top = '-10px'
+        rightWrap.style.top = '30px'
 
     }
 
